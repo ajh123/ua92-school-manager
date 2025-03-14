85cd674af534be3c3523c6939ed44a54a5ea5707
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-03-14 12:35:00 +0000

    fix: prevent SQL injection in table render

--- src/utils/forms.php
+++ src/utils/forms.php
@@ -39,50 +39,76 @@ function get_value($key) {
     return $_SESSION["values"][$key];
 }
 
-function echo_table($name, $editable=true) {
-    global $conn;
-    $sql = "SELECT * FROM $name";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $all = false;
-    
-    if ($result->num_rows > 0) {
-        $all = $result->fetch_all(MYSQLI_ASSOC);
-    }
-    
-    if ($all != false) {
-        $keys = array_keys($all[0]);
-        
-        echo "<div class='alert alert-success' role='alert'>MySQL qurey was successfull. $result->num_rows rows were returned. </div>";
-        
-        if ($editable) {
-            echo "<button type='button' class='btn btn-light'>Insert into $name</button>";
-        }
+const allowed_tables = [
+    "Roles",
+    "ContactInformation",
+    "MedicalInformation",
+    "Users",
+    "Groups",
+    "GroupAssignments",
+    "Salaries",
+    "GuardianAssignments",
+    "Classes",
+    "ClassAssignments",
+    "Attendance",
+    "Books",
+    "BookAssignments",
+    "SiteSettings"
+];
 
-        echo "<table class='table table-striped table-sm'>";
-        echo "<thead>";
-        echo "<tr>";
-        foreach ($keys as $key) {
-            echo "<th scope='col'>$key</th>";
-        }
-        echo "<th scope='col'>Operations</th>";
-        echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
-        foreach ($all as $row) {
-            echo "<tr>";
-            foreach ($row as $row_col) {
-                echo "<td>$row_col</td>";
-            }
-            if ($editable) {
-                echo "<td><a>Edit</a> | <a>Delete</a></td>";
-            }
-            echo "</tr>";
-        }
-        echo "</tbody>";
-        echo "</table>";
+function echo_table($name, $editable=true) {
+    if (!in_array($name, allowed_tables)) {
+        echo "<div class='alert alert-danger' role='alert'>You are not allowed to access that table.</div>";
     } else {
-        echo "<div class='alert alert-warning' role='alert'>MySQL returned an empty response.</div>";
+        global $conn;
+
+        $sql = "SELECT * FROM $name";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $all = false;
+        
+        if ($result->num_rows > 0) {
+            $all = $result->fetch_all(MYSQLI_ASSOC);
+        }
+        
+        if ($all != false) {
+            $keys = array_keys($all[0]);
+            
+            echo "<div class='alert alert-success' role='alert'>MySQL qurey was successfull. $result->num_rows rows were returned. </div>";
+            
+            if ($editable) {
+                echo "<button type='button' class='btn btn-light'>Insert into $name</button>";
+            }
+
+            echo "<table class='table table-striped table-sm'>";
+            echo "<thead>";
+            echo "<tr>";
+            foreach ($keys as $key) {
+                echo "<th scope='col'>$key</th>";
+            }
+            echo "<th scope='col'>Operations</th>";
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            foreach ($all as $row) {
+                echo "<tr>";
+                foreach ($row as $row_col) {
+                    echo "<td>$row_col</td>";
+                }
+                if ($editable) {
+                    echo "<td><a>Edit</a> | <a>Delete</a></td>";
+                }
+                echo "</tr>";
+            }
+            echo "</tbody>";
+            echo "</table>";
+        } else {
+            echo "<div class='alert alert-warning' role='alert'>MySQL returned an empty response.</div>";
+
+            if ($editable) {
+                echo "<button type='button' class='btn btn-light'>Insert into $name</button>";
+            }
+        }
     }
 }
