c21dd7ad55875bbbe551d975b316edadbc8d38de
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-04-10 09:52:34 +0100

    feat: bettter homepage

--- src/public/index.php
+++ src/public/index.php
@@ -12,8 +12,43 @@ $u = $_SESSION["user"];
 ?>
 
 <?php begin_layout("Home"); ?>
-<h2>Home</h2>
-<div class="table-responsive small">
-
+<div class="container text-center">
+    <h2>Manage</h2>
+    <div class="row row-cols-2 row-cols-md-4 g-3">
+    <?php
+        foreach (allowed_tables as $table) {
+            if (has_permission($u, strtolower("table.$table.view"))) {
+                echo "<div class='col'><a class='btn btn-primary w-100' href='/query.php?table=$table'>Manage $table</a></div>";
+            }
+        }
+    ?>
+  </div>
+</div>
+<div class="container text-center">
+    <h2>Your information</h2>
+    <table class="table">
+        <tbody>
+            <tr>
+                <th scope="row">Name</th>
+                <td><?php echo $u["name"] ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Email</th>
+                <td><?php echo $u["email"] ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Role</th>
+                <td><?php echo $u["roleID"] ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Contact Information</th>
+                <td><?php echo $u["contactID"] ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Medical Information</th>
+                <td><?php echo $u["medicalID"] ?></td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 <?php end_layout(); ?>
